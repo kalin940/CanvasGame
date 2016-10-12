9aec6df75b3f867c09840b27c81f5f250007ca47
Kalin <kalinpetrov@dir.bg>
2016-10-12 18:56:14 +0300

    beer boost

--- app.js
+++ app.js
@@ -14,14 +14,15 @@ function init() {
     ctx.font='24px arial white';
     let hero={x:400,y:500};
     let beer={x:0,y:0};
-    let booster = {x:100,y:100};
+    let booster = {x:100,y:400};
     let dirY=true;
     let score=-1;
     let lives=3;
     let running=true;
     let jumping=false;
-    let boosterBool = false;
     let jumpHeight=60;
+    let boosterBool = false;
+    let boosterTaken=false;
     window.addEventListener('keydown',kebdHandler);
     
     function kebdHandler(event) {
@@ -44,6 +45,7 @@ function init() {
         }
        // update();
     }
+
     function jump(){
         if (!jumping) {
             jumping = true;
@@ -53,12 +55,33 @@ function init() {
     function land() {
         jumping = false;
     }
+    function boosterReset() {
+        booster.x=Math.round(Math.random()*720);
+        moveSpeed+=2;
+        boosterBool=false;
+        boosterTaken=true;
+    }
+    setInterval(boostDraw,2600)
+    function boostDraw() {
+        boosterBool = !boosterBool;
+        booster.x=Math.round(Math.random()*720);
+    }
+
     function  update() {
         moveBeer();
         let X=(hero.x+50)-(beer.x+40);
         let Y=(hero.y+50)-(beer.y+40);
-//to do
+        let Xboost=(hero.x+50)-(booster.x+30);
+        let Yboost=(hero.y+50)-(booster.y+30);
+        if(jumping){
+            Y=(hero.y+50-jumpHeight)-(beer.y+40);
+            Yboost=(hero.y+50-jumpHeight)-(booster.y+30);
+        }
         let distance=Math.sqrt(X*X + Y*Y);
+        let distanceWithBooster=Math.sqrt(Xboost*Xboost + Yboost*Yboost);
+        if(distanceWithBooster<69){
+            boosterReset();
+        }
         if(distance<97){
             reset();
         }
@@ -68,23 +91,25 @@ function init() {
         beer.x=Math.round(Math.random()*720);
         beer.y=0;
         score++;
-        if(score%5==0){
+        if(score%7==0){
             beerSpeed++;
         }
-        if(score%3 ==1){
+        if(boosterBool && !boosterTaken){
             boosterBool = true;
         }
         else{
             boosterBool = false;
+            boosterTaken=false;
         }
     }
+
     function draw() {
         let y=hero.y;
         ctx.clearRect(0,0,800,600);
 
         ctx.drawImage(backgroundImg,0,0,800,600);
         if(boosterBool){
-            ctx.drawImage(boosterImg,0,0,100,100)
+            ctx.drawImage(boosterImg,booster.x,booster.y,70,70)
         }
         ctx.drawImage(beerImg,beer.x,beer.y,80,80);
         if(jumping){
@@ -93,7 +118,6 @@ function init() {
         }else{
             ctx.drawImage(heroImg,hero.x,hero.y,100,100);
         }
-
         ctx.fillText(`Beers:${score}`,10,30);
         ctx.fillText(`Lives:${lives}`,10,50);
     }
